7e62de5758d98b4817f72fda2145612f6ff2257e
Lara Penz <lara_penz@yahoo.de>
2019-01-23 19:27:01 +0100

    Die cryptotools

--- app/src/main/java/edu/kit/nfcrypto/activities/ActivityCryptotools.java
+++ app/src/main/java/edu/kit/nfcrypto/activities/ActivityCryptotools.java
@@ -10,6 +10,6 @@ public class ActivityCryptotools extends ActivityBase {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_cryptotools);
+        setContentView(R.layout.activity_cryptotools_ase);
     }
 }
