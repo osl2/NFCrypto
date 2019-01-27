141e1ce98beff2911822a83c902f664607ee9122
Annika <annika.vielsack@student.kit.edu>
2019-01-27 22:54:13 +0100

    Personenwahl aufgehübscht

--- app/src/main/java/edu/kit/nfcrypto/activities/ActivityPersons.java
+++ app/src/main/java/edu/kit/nfcrypto/activities/ActivityPersons.java
@@ -3,6 +3,7 @@ package edu.kit.nfcrypto.activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v7.widget.CardView;
 import android.view.View;
 import android.widget.Button;
 
@@ -16,19 +17,20 @@ public class ActivityPersons extends ActivityBase {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_persons);
 
-        final Button buttonAlice = findViewById(R.id.activity_persons_button_alice);
+        final CardView buttonAlice = findViewById(R.id.activity_persons_button_alice);
         buttonAlice.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 ActivityPersons.this.startActivity(new Intent(ActivityPersons.this, ActivityAlice.class));
             }
         });
-        final Button buttonBob = findViewById(R.id.activity_persons_button_bob);
+
+        final CardView buttonBob = findViewById(R.id.activity_persons_button_bob);
         buttonBob.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 ActivityPersons.this.startActivity(new Intent(ActivityPersons.this, ActivityBob.class));
             }
         });
-        final Button buttonEve = findViewById(R.id.activity_persons_button_eve);
+        final CardView buttonEve = findViewById(R.id.activity_persons_button_eve);
         buttonEve.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 ActivityPersons.this.startActivity(new Intent(ActivityPersons.this, ActivityEve.class));
