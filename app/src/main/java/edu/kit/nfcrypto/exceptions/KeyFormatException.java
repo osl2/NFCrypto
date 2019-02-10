dd6b5e69c8176e7c68ede239b2a3fb84db34d8f3
Annika <annika.vielsack@student.kit.edu>
2019-02-10 13:16:01 +0100

    AES läuft

--- app/src/main/java/edu/kit/nfcrypto/exceptions/KeyFormatException.java
+++ app/src/main/java/edu/kit/nfcrypto/exceptions/KeyFormatException.java
@@ -5,6 +5,7 @@ public class KeyFormatException extends IllegalArgumentException {
     private String message;
     public static final String KEY_DATA = "KeyData not valid.";
     public KeyFormatException(String message){
+        super(message);
         this.message = message;
     }
 }
