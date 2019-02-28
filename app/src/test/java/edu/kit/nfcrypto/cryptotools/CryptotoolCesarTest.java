2709bbd7ce111f097e79bd797ea97dcc28a00335
Lara Penz <lara_penz@yahoo.de>
2019-02-28 13:22:16 +0100

    Testfälle für Cäsart key und Cryptotool angefangen randfälle fehlen noch

--- app/src/test/java/edu/kit/nfcrypto/cryptotools/CryptotoolCesarTest.java
+++ app/src/test/java/edu/kit/nfcrypto/cryptotools/CryptotoolCesarTest.java
@@ -5,12 +5,30 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class CryptotoolCesarTest {
+    //##############################################################################################
+    //Hier wird decrypt getestet
+    //##############################################################################################
 
     @Test
     public void decrypt() {
+        CryptotoolCesar cesar = new CryptotoolCesar(0);
+        String testString = "Hallo Welt";
+        String decryptString = "Hallo Welt";
+        assertEquals("",cesar.decrypt(testString),decryptString );
     }
+
+
+    //##############################################################################################
+    //Hier wird crack getestet
+    //##############################################################################################
 
     @Test
     public void crack() {
+        CryptotoolCesar cesar = new CryptotoolCesar();
+        String testString = "Hallo Welt";
+        String crackedString = "Hallo Welt";
+        String help = "Entschlüsselt";
+        assertEquals("", crackedString,cesar.crack(testString,help) );
+
     }
 }
--- app/src/test/java/edu/kit/nfcrypto/keys/CesarKeyTest.java
+++ app/src/test/java/edu/kit/nfcrypto/keys/CesarKeyTest.java
@@ -5,16 +5,27 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class CesarKeyTest {
+//##############################################################################################
+    //Hier wird encrypt getestet
+    //##############################################################################################
 
     @Test
     public void encrypt() {
+        CesarKey testKey = new CesarKey("0");
+        String testString = "ABCDE";
+        String string = "ABCDE";
+        assertEquals("Der Text wird nicht richtig encrypted",testKey.encrypt(testString),string);
     }
+//##############################################################################################
+    //Hier wird decrypt getestet
+    //##############################################################################################
 
     @Test
     public void decrypt() {
+        CesarKey testKey = new CesarKey("0");
+        String testString = "ABCDE";
+        String string = "ABCDE";
+        assertEquals("Der Text wird nicht richtig encrypted",testKey.decrypt(testString),string);
     }
 
-    @Test
-    public void getKeyData() {
-    }
 }
